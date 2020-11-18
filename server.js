b2a92583258a56bbfb8e8f4ad846207f2e5ab13d
git99.src <cpu.al2@gmail.com>
2020-11-18 12:10:18 -0800

    Added API route folder

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const authController = require("./controllers/auth.controller");
+const routes = require("./routes/api/api");
+
 const { handleErrors } = require("./middleware/error.middleware");
-const { hasValidToken } = require("./middleware/auth.middleware");
+
 
 if (!process.env.SERVER_SECRET) {
   // SERVER_SECRET env var is required for auth
@@ -25,25 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "client/build")));
 
-// routing
-app.post("/api/auth/login", authController.login);
-app.post("/api/auth/signup", authController.signup);
-
-// example of unprotected route. (guest users can access)
-app.get("/api/unprotected", (req, res) => res.json({ message: "public data" }));
-
-// example of a protected route. Request must have a valid token.
-app.get("/api/protected", hasValidToken, (req, res) => {
-  // the hasValidToken middleware decodes the payload and adds a "user" property to the request.
-  console.log(req.user);
-  res.json({ message: "protected data" });
-});
+// Define API routes here
+app.use(routes);
 
 app.use(handleErrors);
-
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client/build/index.html"))
-);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`server listening on ${port}`));
