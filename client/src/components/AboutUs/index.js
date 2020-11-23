9c95a3b554ffb405a0bccd06e04dfc5fb5f9ec84
Stefanie Serpa <smserpa@gmail.com>
2020-11-23 14:20:17 -0800

    removed col from AboutUs, present in HomePage

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -10,15 +10,12 @@ function AboutUs() {
         });
       }, []);
       return (
-        <div className="col-sm">
           <div className="card">
             <h2 className="about-title"> Effortlessly organize and track your day to day</h2>
             <p className="about-text">Manage your projects better by creating timed tasks that you can store and pick up where you left off anytime</p>
             <br />
             <p className="about-text">Like a wandering Hedge Knight of the Seven kingdoms, free-lance developers work to the beat of their own drum but we're here to provide you with some assistance along the way</p>
           </div>
-        </div>
-        
       );
 }
 
