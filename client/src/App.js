4c6fa5f514678240f449f7d880d8cc21ef7c51cd
Hunter Kantner <hunterkantner@gmail.com>
2020-11-23 13:34:04 -0800

    added log in and fixed rows of homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,26 @@ import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import Navbar from "./components/Navbar";
-import HistoryCard from "./components/History"
+import HistoryCard from "./components/History";
 import ProtectedExamplePage from "./components/ProtectedExamplePage";
 import { ProvideAuth } from "./util/authContext";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Logo from "./components/Logo/hedgenightlogo.png"
+import Logo from "./components/Logo/hedgenightlogo.png";
 import GoT from "./components/GoTblock";
 import Footer from "./components/Footer";
-
 
 function App() {
   return (
     <ProvideAuth>
       <Router>
-      <div className="row align-items-center"> 
-      <div className="col-8 float-left">
-      <img src={Logo} alt="hedgenight" /></div>
-      <div className="col-4 float-right">
-        <GoT /></div>
-        </div> 
-        <img src={Logo} alt="hedgenight" />
-        <GoT />
-    
+        <div className="row align-items-center">
+          <div className="col-8 float-left">
+            <img src={Logo} alt="hedgenight" />
+          </div>
+          <div className="col-4 float-right">
+            <GoT />
+          </div>
+        </div>
         <Navbar />
         <Switch>
           <Route exact path="/">
@@ -44,7 +42,8 @@ function App() {
             <HistoryCard />
           </ProtectedRoute>
         </Switch>
-      </Router><Footer /> 
+      </Router>
+      <Footer />
     </ProvideAuth>
   );
 }
--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -3,6 +3,7 @@ import API from "../../util/API";
 import "./styles.css";
 import Timer from "../Timer";
 import AboutUs from "../AboutUs";
+import LoginPage from "../LoginPage";
 
 
 function HomePage() {
@@ -13,9 +14,16 @@ function HomePage() {
     });
   }, []);
   return (
-    <div className="timer">
+    <div className="row">
+    <div className="col-4 timer mx-auto">
       <Timer />
+      </div>
+      <div className="col-4 mx-auto">
       <AboutUs />
+      </div>
+      <div className="col-4 mx-auto">
+      <LoginPage />
+      </div>
     </div>
   );
 }
