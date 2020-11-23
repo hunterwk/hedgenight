8fdb08058f98143484860c7b76133d5d1da13e56
Marylee Pabst <marylee@uconn.edu>
2020-11-23 03:41:32 -0800

    api stuff

--- client/src/components/History/index.js
+++ client/src/components/History/index.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../../util/authContext";
 import API from "../../util/API";
+import './styles.css';
+import { Card } from "react-bootstrap";
 
 function ProtectedExamplePage() {
     const { logout, user } = useAuth();
@@ -18,19 +20,51 @@ function ProtectedExamplePage() {
 
 
     return (
-        <div>
-            <h1>History Card</h1>
-            <p>{user.tasks.id}</p>
-            <p>{user.tasks.notes}</p>
-            <p>{user.tasks.duration}</p>
-            <h3>Protected API Data Example</h3>
+        <div className="HistoryCard">
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Title: {user.tasks.id}</Card.Title>
+                        <Card.Text>
+                            Notes: {user.tasks.notes}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Session time: {user.tasks.duration}</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Title: {user.tasks.id}</Card.Title>
+                        <Card.Text>
+                            Notes: {user.tasks.notes}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Session time: {user.tasks.duration}</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Title: {user.tasks.id}</Card.Title>
+                        <Card.Text>
+                            Notes: {user.tasks.notes}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Session time: {user.tasks.duration}</small>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
             {data.length === 0
-                ? <h3 className="text-center">WELCOME NEWCOMER</h3>
+                ? <h3 className="text-center">fast stats</h3>
                 : data.map((tasks) => (
-                    <p key={tasks._id.toString()} onClick={() => API.loadTasks(tasks._id.toString())}>{tasks.name}<br />{tasks.duration}<br />{tasks.notes}<br /></p>))}
+                    <p key={tasks._id.toString()} onClick={() => API.loadTasks(tasks._id.toString())}>{tasks.name}<br />{tasks.duration}<br />{tasks.notes}<br /></p>));
         </div>
     );
-}
 
-export default ProtectedExamplePage;
+    export default ProtectedExamplePage;
 // {data && <pre>{JSON.stringify(data)}</pre>}
