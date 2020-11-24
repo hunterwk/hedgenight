d25e48bd43fbfe382150d3d88002131ecdd6eced
Hunter Kantner <hunterkantner@gmail.com>
2020-11-23 20:49:17 -0800

    added timertext component

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -7,22 +7,19 @@ import LoginPage from "../LoginPage";
 import HistoryPage from "../History";
 import { useAuth } from "../../util/authContext"
 import RecentHistory from "../RecentHistory";
+import 'bootstrap/dist/css/bootstrap.css';
+import TimerText from "../TimerText";
 
 function HomePage(props) {
   const { isLoggedIn } = useAuth()
-  // const [data, setData] = useState(null);
-  // useEffect(() => {
-  //   API.getPublicExample().then((response) => {
-  //     setData(response.data);
-  //   });
-  // }, []);
+
   return (
     <div className="row">
     <div className="col-4 timer mx-auto">
       <Timer useTimer={props.useTimer}/>
       </div>
       <div className="col-4 mx-auto">
-      {!isLoggedIn && <AboutUs />}  
+      {isLoggedIn ? <TimerText useTimer={props.useTimer}/> : <AboutUs />}  
       </div>
       <div className="col-4 mx-auto">
         {isLoggedIn ? <RecentHistory /> : <LoginPage />}
--- client/src/components/RecentHistory/index.js
+++ client/src/components/RecentHistory/index.js
@@ -1,51 +1,49 @@
 import { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import API from "../../util/API";
-import '../HomePage/styles.css';
+import "../HomePage/styles.css";
 import "./styles.css";
 
 const TimeFormat = require("hh-mm-ss");
 
-
-
 function RecentHistory(props) {
   const [data, setData] = useState([]);
-  const history = useHistory()
+  const history = useHistory();
   useEffect(() => {
     API.findTasks().then(({ data }) => {
-      let newData = data.slice(Math.max(data.length - 4,0))
+      let newData = data.slice(Math.max(data.length - 4, 0));
       setData(newData);
-      console.log(newData)
-      console.log(data[data.length -2])
+      console.log(newData);
+      console.log(data[data.length - 2]);
     });
   }, []);
 
   return (
-    
     <div className="HistoryCard container mx-auto">
       <h3 className="text-align-center"> Recent Sessions:</h3>
+      <Link to="/history">View all previous sessions</Link>
       <div className="row">
-        
         {data.length === 0 ? (
           <h4 className="text-center">You dont have any previous sessions!</h4>
         ) : (
-          data.map((tasks) => 
-            <div className="col-12"key={tasks._id.toString()}>
-            <div className="card">
-              <div className="card-body">
-                <h4 className="card-title">Title: {tasks.name}</h4>
-                <h5 className="card-text">
-                  Duration: {TimeFormat.fromS(tasks.duration)}
-                </h5>
-                <p className="card-text">
-                  Notes:
-                  <br />
-                  {tasks.notes}
-                </p>
+          data.map((tasks) => (
+            <div className="col-12" key={tasks._id.toString()}>
+              <div className="card">
+                <div className="card-body">
+                  <h4 className="card-title">Title: {tasks.name}</h4>
+                  <h5 className="card-text">
+                    Duration: {TimeFormat.fromS(tasks.duration)}
+                  </h5>
+                  <p className="card-text">
+                    Notes:
+                    <br />
+                    {tasks.notes}
+                  </p>
+                </div>
               </div>
             </div>
-            </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
