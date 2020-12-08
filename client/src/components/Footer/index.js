bfe34f155c6d77548835983ff0949bc5696c4d0f
Hunter Kantner <hunterkantner@gmail.com>
2020-12-07 21:22:30 -0800

    fixed footer to absolute bottom so that it wasnt ruining mobile responsiveness

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return(
-        <div className="footer fixed-bottom">
+        <div className="footer">
             <span> HedgeNight was hand coded by 4 awesome folks in a coding bootcamp. <a href="https://github.com/git99-src/hedgenight">Github.</a></span>
         </div>
     )
